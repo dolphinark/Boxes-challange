2e13c2fbbde733cc25dff9bfc8fc3a3ca7794f82
Rita <ritahung19@gmail.com>
2022-05-22 16:51:36 +0800

    box challenge

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Box from "./Box";
+import boxes from "./boxes";
+import "./style.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(props) {
+  const [squares, setSquares] = useState(boxes);
+
+  function toggle(id) {
+    setSquares((prevSquare) => {
+      return prevSquare.map((square) => {
+        return square.id === id ? { ...square, on: !square.on } : square;
+      });
+    });
+  }
+
+  const squareElements = squares.map((square) => (
+    <Box 
+    key={square.id} 
+    id={square.id} 
+    on={square.on} 
+    toggle={toggle} 
+    // toggle={()=>toggle(square.id)} then don't need id={square.id}
+    />
+  ));
+
+  return <main>{squareElements}</main>;
 }
 
 export default App;
